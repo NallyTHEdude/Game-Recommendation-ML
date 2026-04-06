dd996b6322947300463e5d1785e77c16fd46ba4f
NallyTHEdude <rifaqatnawaz25@gmail.com>
2026-04-06 19:27:54 +0530

    commented init_db with additional readable comments

--- server/src/app/main.py
+++ server/src/app/main.py
@@ -3,6 +3,7 @@ from utils import logger
 from db import connect_db, init_db
 
 if __name__ == "__main__":
-    init_db()
+    # uncomment in production to initialize the database on startup
+    #init_db()
     database = connect_db()
     logger.info("Application started successfully.")
