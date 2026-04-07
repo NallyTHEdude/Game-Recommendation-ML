40830830b829c142d500e305610f29f8bb0c1ec6
NallyTHEdude <rifaqatnawaz25@gmail.com>
2026-04-07 11:48:19 +0530

    added singleton

--- server/src/app/_core/config.py
+++ server/src/app/_core/config.py
@@ -1,24 +1,36 @@
 import os 
 from dotenv import load_dotenv
 from typing import Final, Optional
-from dataclasses import dataclass
 
 load_dotenv()
 
-@dataclass(frozen=True)
-class _Settings():
-    # Define all environment variables with default values
-    ENVIRONMENT : Final[str] = os.getenv('ENVIRONMENT', 'development')
-    PORT : Final[int] = int(os.getenv('PORT') or 3000)
-    SERVER_HOST : Final[str] = os.getenv('SERVER_HOST', 'http://localhost')
+class Settings():
+    _config_instance = None
+    _initialized = False
 
-    # Handle database variables
-    DATABASE_URL : Final[str] = os.getenv('DATABASE_URL')
+    def __new__(cls):
+        if cls._config_instance is None:
+            cls._config_instance = super().__new__(cls)
+        return cls._config_instance
 
-    # Handle RAWG API variables
-    RAWG_API_KEY : Final[Optional[str]] = os.getenv('RAWG_API_KEY')
+    def __init__(self):
+        if self._initialized:
+            return
+        # Define all environment variables with default values
+        self.ENVIRONMENT : Final[str] = os.getenv('ENVIRONMENT', 'development')
+        self.PORT : Final[int] = int(os.getenv('PORT') or 3000)
+        self.SERVER_HOST : Final[str] = os.getenv('SERVER_HOST', 'http://localhost')
 
-    def __post_init__(self):
+        # Handle database variables
+        self.DATABASE_URL : Final[str] = os.getenv('DATABASE_URL')
+
+        # Handle RAWG API variables
+        self.RAWG_API_KEY : Final[Optional[str]] = os.getenv('RAWG_API_KEY')
+
+        self._handle_errors()
+        self._initialized = True
+    
+    def _handle_errors(self):
         if self.DATABASE_URL is None:
             raise KeyError("DATABASE_URL environment variable is required")
         if self.RAWG_API_KEY is None:
@@ -26,4 +38,4 @@ class _Settings():
 
 
 # export the config object
-config = _Settings()
+config = Settings()
