8c9d80a66fd47e7387c6f0a480735492d34945cf
NallyTHEdude <rifaqatnawaz25@gmail.com>
2026-04-08 14:23:34 +0530

    added comments

--- server/src/app/main.py
+++ server/src/app/main.py
@@ -7,4 +7,6 @@ if __name__ == "__main__":
     # uncomment in production to initialize the database on startup
     #init_db()
     database = connect_db()
+    # embedding_model = ml_models.embedding_model
+    # print(embedding_model.encode_passage(["action", "adventure"], ["RPG"], "An epic quest in a fantasy world."))
     logger.info("Application started successfully.")
